61f5c48e7714231e319011f37d66d2fb934e340e
Emilia Miaskiewicz <emiaskiewicz21@gmail.com>
2025-05-14 17:22:15 +0200

    dodanie tiles dict i list oraz metody is_available

--- board.py
+++ board.py
@@ -6,34 +6,58 @@ import string
 
 
 class Board:
-    sets_data=None
-    tiles=[]
+    sets_data = None
+    tiles_dict: dict[Vector, Tile] = {}  # dostep do kafelkow po pozycji - wyszukiwanie O(1)
+    tiles_list = []
 
-    def __init__(self,set_name):
-        '''self.tiles = []
-        df = pandas.read_json(f"game_sets/uklady.json", orient='values')
-        for pos in df['positions']:
-            self.tiles.append(Tile(color=random.choice(colors),
-             figure=random.choice(figures),
-             position=Vector(pos[0],pos[1],pos[2])))'''
-        with open(f"game_sets\\uklady.json",mode="r",encoding="utf-8") as file:
-            self.sets_data=json.load(file)
+    def __init__(self, set_name):
+        with open(f"game_sets\\uklady.json", mode="r", encoding="utf-8") as file:
+            self.sets_data = json.load(file)
 
         for set in self.sets_data["sets"]:
-            if set["name"]==set_name:
+            if set["name"] == set_name:
                 for pos in set["positions"]:
-                    self.tiles.append(Tile(color=random.choice(colors),
-                                           figure=random.choice(figures),
-                                           position=Vector(pos[0], pos[1], pos[2]),
-                                           points=0))
+                    position = Vector(pos[0], pos[1], pos[2])
+                    tile = Tile(
+                        color=random.choice(colors),
+                        figure=random.choice(figures),
+                        position=position,
+                        points=0
+                    )
+                    self.tiles_dict[position] = tile
+                    self.tiles_list.append(tile)
 
-    def find_on_board(self,_position):
-        for tile in self.tiles:
-            if tile.position == _position:
-                return f"Tile on position {_position} exists"
-        return f"Tile on position {_position} does not exists"
+    def find_on_board(self, _position):
+        if self.tiles_dict.get(Vector(_position.x, _position.y, _position.z)):
+            return f"Tile on position {_position} exists"
+        else:
+            return f"Tile on position {_position} does not exists"
 
+    def is_available(self, tile: Tile):
+        pos = tile.position  # current tile position
+        #jesli nie istnieje dana pozycja
+        if pos not in self.tiles_dict:
+            return False
+        #jesli nad obecnym kafelkiem istnieje kafelek to od razu False
+        if pos.above() in self.tiles_dict:
+            return False
+
+        #sprawdzenie ilosci wolnych scian (przynajmniej dwie musza byc wolne)
+        free_sides = 0
+        if pos.left() not in self.tiles_dict:
+            free_sides += 1
+        if pos.right() not in self.tiles_dict:
+            free_sides += 1
+        if pos.up() not in self.tiles_dict:
+            free_sides += 1
+        if pos.down() not in self.tiles_dict:
+            free_sides += 1
+
+        return free_sides >= 2
 
 board = Board("Kopiec")
 
-print(board.find_on_board(Vector(1,4,2)))
+#print(board.find_on_board(Vector(1, 30, 2)))
+#testtile=Tile('red','dot 7',Vector(0,0,0),0)
+
+#print(board.is_available(testtile))
